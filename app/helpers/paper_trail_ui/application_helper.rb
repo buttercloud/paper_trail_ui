bdc3edec0d825844767608df8787e36cd935ded8
Nayef Muhiar <nayef@buttercloud.com>
2017-06-08 17:31:42 +0300

    Edit index view to provide ransack search form, add show page for PaperTrail version object, add object_info_row partial to be used to render PaperTrail object details, add helper for row counter with paging

--- app/helpers/paper_trail_ui/application_helper.rb
+++ app/helpers/paper_trail_ui/application_helper.rb
@@ -1,4 +1,10 @@
 module PaperTrailUi
   module ApplicationHelper
+
+    def row_counter_with_paging(index, options={})
+      current_page = (params[options[:page_param] || :page] || 1).to_i
+      per_page = options[:per_page] || WillPaginate.per_page
+      ((current_page-1)*per_page)+index+1
+    end
   end
 end
