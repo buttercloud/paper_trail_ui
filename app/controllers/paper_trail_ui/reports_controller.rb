1aabb43af0d9ff8637833bf268e6435908ff662b
Nayef Muhiar <nayef@buttercloud.com>
2017-06-08 17:29:20 +0300

    Require YAML, modify index method to use ransak to query, add show action, add global variables to load model and event names

--- app/controllers/paper_trail_ui/reports_controller.rb
+++ app/controllers/paper_trail_ui/reports_controller.rb
@@ -1,14 +1,21 @@
 require 'will_paginate/array'
+require 'yaml'
 require_dependency "paper_trail_ui/application_controller"
 
 module PaperTrailUi
   class ReportsController < ApplicationController
+    MODEL_NAMES = PaperTrail::Version.select("DISTINCT item_type").map(&:item_type)
+    EVENT_TYPES = PaperTrail::Version.select("DISTINCT event").map(&:event)
     def index
-      @model_names = PaperTrail::Version.select("DISTINCT item_type").map(&:item_type)
+      @model_names = {"All" => ""}.merge(MODEL_NAMES.reduce({}){|a, x| a[x] = x; a})
+      @event_names = {"All" => ""}.merge(EVENT_TYPES.reduce({}){|a, x| a[x] = x; a})
+      @query = PaperTrail::Version.search params[:q]
+      @versions = @query.result(distinct: true).paginate(page: params[:page], per_page: 100)
     end
 
     def show
-      
+      @version = PaperTrail::Version.find params[:id]
+      @object = YAML.load(@version.object)
     end
   end
 end
