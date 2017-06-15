a4d928f45e4b53fc59c17ca5eb1cf36ce305a536
Nayef Muhiar <nayef@buttercloud.com>
2017-06-15 16:06:44 +0300

    Add method to generate a hash from array to be used in converting model names, events, and actors to filter hashes

--- app/controllers/paper_trail_ui/reports_controller.rb
+++ app/controllers/paper_trail_ui/reports_controller.rb
@@ -6,16 +6,24 @@ module PaperTrailUi
   class ReportsController < ApplicationController
     MODEL_NAMES = PaperTrail::Version.select("DISTINCT item_type").map(&:item_type)
     EVENT_TYPES = PaperTrail::Version.select("DISTINCT event").map(&:event)
+    ACTOR_NAMES = PaperTrail::Version.select("DISTINCT whodunnit").map(&:whodunnit)
+
     def index
-      @model_names = {"All" => ""}.merge(MODEL_NAMES.reduce({}){|a, x| a[x] = x; a})
-      @event_names = {"All" => ""}.merge(EVENT_TYPES.reduce({}){|a, x| a[x] = x; a})
+      @model_names = array_to_filter_hash(MODEL_NAMES)
+      @event_names = array_to_filter_hash(EVENT_TYPES)
+      @actor_names = array_to_filter_hash(ACTOR_NAMES)
       @query = PaperTrail::Version.search params[:q]
       @versions = @query.result(distinct: true).paginate(page: params[:page], per_page: 100)
     end
 
     def show
       @version = PaperTrail::Version.find params[:id]
-      @object = YAML.load(@version.object)
+      @object = YAML.load(@version.object || "---") || {}
     end
+
+    private
+      def array_to_filter_hash(array)
+        {"All" => ""}.merge(array.reduce({}){|a, x| a[x] = x; a})
+      end
   end
 end
