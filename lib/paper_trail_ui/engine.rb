3aaeadad38094a8ed2dd99543da56939249ada9b
Nayef Muhiar <nayef@buttercloud.com>
2017-06-22 18:28:04 +0300

    Edit rakefile and engine rb to accomodate rspec testing and load correct files

--- lib/paper_trail_ui/engine.rb
+++ lib/paper_trail_ui/engine.rb
@@ -2,5 +2,9 @@ module PaperTrailUi
   class Engine < ::Rails::Engine
     require 'jquery-rails'
     isolate_namespace PaperTrailUi
+
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
